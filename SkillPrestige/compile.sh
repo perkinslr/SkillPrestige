08c4caf1fcfd4bd58cb560fa8a2b87525473294a
perkinslr <perkins@github.alestan.publicvm.com>
2018-08-11 10:41:06 -0700

    Fix build system

--- SkillPrestige/compile.sh
+++ SkillPrestige/compile.sh
@@ -1,4 +1,6 @@
 mkdir -p ../build/SkillPrestige
 mcs -unsafe `find . | grep cs$` -target:library -out:../build/SkillPrestige/SkillPrestige.dll -r:../stardew_valley/StardewValley.exe -r:../stardew_valley/StardewModdingAPI.exe -r:../stardew_valley/MonoGame.Framework.dll -r:../stardew_valley/Newtonsoft.Json.dll
-cp manifest.json ../build/SkillPrestige
+cp *.png ../build/SkillPrestige
+cp *.json ../build/SkillPrestige
 mkdir -p ../build/SkillPrestige/data
+
