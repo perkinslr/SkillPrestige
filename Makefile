be1b2f9a6c68d006754046c959ee9167250dfc7f
perkinslr <perkins@github.alestan.publicvm.com>
2018-08-11 10:31:43 -0700

    Remove luck skill support Add linux makefiles

---
+++ Makefile
@@ -0,0 +1,6 @@
+all: stardew_valley
+	$(MAKE) -C SkillPrestige
+
+stardew_valley:
+	$(error Please symlink your stardew valley game folder to ./stardew_valley)
+
---
+++ SkillPrestige/compile.sh
@@ -0,0 +1,4 @@
+mkdir -p ../build/SkillPrestige
+mcs -unsafe `find . | grep cs$` -target:library -out:../build/SkillPrestige/SkillPrestige.dll -r:../stardew_valley/StardewValley.exe -r:../stardew_valley/StardewModdingAPI.exe -r:../stardew_valley/MonoGame.Framework.dll -r:../stardew_valley/Newtonsoft.Json.dll
+cp manifest.json ../build/SkillPrestige
+mkdir -p ../build/SkillPrestige/data
