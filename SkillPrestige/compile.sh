1ca29aabd8fd8f09893d93408e5e548052d88fd3
Logan Perkins <perkins@github.alestan.publicvm.com>
2018-08-13 15:36:46 -0700

    Output built mod to build/SkillPrestige

--- SkillPrestige/compile.sh
+++ SkillPrestige/compile.sh
@@ -1,1 +1,6 @@
-mcs -unsafe `find . | grep cs$` -target:library -out:./SkillPrestige.dll -r:./stardew_valley/StardewValley.exe -r:./stardew_valley/StardewModdingAPI.exe -r:./stardew_valley/MonoGame.Framework.dll -r:../ns/Bin/net45/Newtonsoft.Json.dll
+mkdir -p ../build/SkillPrestige
+mcs -unsafe `find . | grep cs$` -target:library -out:../build/SkillPrestige/SkillPrestige.dll -r:../stardew_valley/StardewValley.exe -r:../stardew_valley/StardewModdingAPI.exe -r:../stardew_valley/MonoGame.Framework.dll -r:../stardew_valley/Newtonsoft.Json.dll
+cp *.png ../build/SkillPrestige
+cp *.json ../build/SkillPrestige
+mkdir -p ../build/SkillPrestige/data
+
